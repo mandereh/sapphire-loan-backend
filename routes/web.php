4b0f21737faf737583d2d0de1894e1bb4c3250a6
Daerego Braide <phronesis4xt@gmail.com>
2024-04-05 21:22:25 +0100

    download lead template functionality

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('leads/template/download',[\App\Http\Controllers\LeadController::class,'downloadLeadsTemplate']);
