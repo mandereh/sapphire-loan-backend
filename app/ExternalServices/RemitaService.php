bd8774a94863f766b8ddd0ba04e5bbdb996a04ca
dicorage <cbekenawei@gmail.com>
2024-04-16 14:31:03 +0100

    maranatha

--- app/ExternalServices/RemitaService.php
+++ app/ExternalServices/RemitaService.php
@@ -294,7 +294,7 @@ class RemitaService
     }
 
     private function dummyData(){
-        $monthlyPay = round(mt_rand(30000, 300000), -3);
+        $monthlyPay = round(mt_rand(200000, 1000000), -3);
         $loanLength = mt_rand(1, 3);
         $repaymentAmount = round(mt_rand(30000, 200000), -3);
 
