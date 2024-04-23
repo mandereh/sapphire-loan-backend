bf2e197daafe9d0ceca0803aedadb2a4043785ee
dicorage <cbekenawei@gmail.com>
2024-04-23 10:33:52 +0100

    get user ippis issue

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -75,7 +75,8 @@ class UserController extends Controller
         //     'ippis' => 'required|numeric'
         // ]);
 
-        $user = User::where('ippis_number', $request->ippis)
+                $user = User::
+                        where('ippis_number', $request->ippis ?? '')
                         ->orWhere('phone_number', $request->phone_number ?? '')
                         ->orWhere('email', $request->email ?? '')
                         ->first();
